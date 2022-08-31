c0187b0b166856537aecbb4fa9071a8ac500beef
NTCNTS <sylinh412@gmail.com>
2022-08-31 09:19:24 +0700

    commit

---
+++ linh_2050531200221_LoginDesign/app/src/main/java/com/example/linh_2050531200221_logindesign/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.linh_2050531200221_logindesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
